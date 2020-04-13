66c96d9840fdf505190f55469781de34b9119152
Tadas <tadas.radz@gmail.com>
2020-04-13 13:28:29 +0300

    first commit

---
+++ src/main/java/com/radsoft/readdictive/ReaddictiveApplication.java
@@ -0,0 +1,13 @@
+package com.radsoft.readdictive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaddictiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaddictiveApplication.class, args);
+	}
+
+}
